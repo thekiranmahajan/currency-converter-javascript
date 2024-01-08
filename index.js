ab5e2cf98607a5e68b88546d628ab8aaa45e2e3b
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-08 11:34:06 +0530

    currency converter is ready to use✅

---
+++ index.js
@@ -0,0 +1,24 @@
+const from = document.getElementById("from");
+const to = document.getElementById("to");
+const inputCurrency = document.getElementById("from-currency");
+const outputCurrency = document.getElementById("to-currency");
+const conversionInfo = document.getElementById("conversion-info");
+
+const exchangeCurrency = async () => {
+  const url = `https://v6.exchangerate-api.com/v6/4454f9cd9a5f86f42e1e0a43/latest/${from.value}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      outputCurrency.value = (
+        inputCurrency.value * data.conversion_rates[to.value]
+      ).toFixed(2);
+      conversionInfo.innerText = `1 ${from.value} = ${
+        data.conversion_rates[to.value]
+      } ${to.value}`;
+    });
+};
+exchangeCurrency();
+
+from.addEventListener("change", exchangeCurrency);
+to.addEventListener("change", exchangeCurrency);
+inputCurrency.addEventListener("input", exchangeCurrency);
