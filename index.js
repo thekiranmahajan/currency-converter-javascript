f3fdff926f474b557107e87135a66401f08ef2e7
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-08 11:39:37 +0530

    added a flag for checking the value of input for non zero and non empty

--- index.js
+++ index.js
@@ -5,17 +5,23 @@ const outputCurrency = document.getElementById("to-currency");
 const conversionInfo = document.getElementById("conversion-info");
 
 const exchangeCurrency = async () => {
-  const url = `https://v6.exchangerate-api.com/v6/4454f9cd9a5f86f42e1e0a43/latest/${from.value}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      outputCurrency.value = (
-        inputCurrency.value * data.conversion_rates[to.value]
-      ).toFixed(2);
-      conversionInfo.innerText = `1 ${from.value} = ${
-        data.conversion_rates[to.value]
-      } ${to.value}`;
-    });
+  if (inputCurrency !== 0 && inputCurrency !== "") {
+    try {
+      const url = `https://v6.exchangerate-api.com/v6/4454f9cd9a5f86f42e1e0a43/latest/${from.value}`;
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          outputCurrency.value = (
+            inputCurrency.value * data.conversion_rates[to.value]
+          ).toFixed(2);
+          conversionInfo.innerText = `1 ${from.value} = ${
+            data.conversion_rates[to.value]
+          } ${to.value}`;
+        });
+    } catch (error) {
+      conversionInfo.innerText = `Oops something went wrong...`;
+    }
+  }
 };
 exchangeCurrency();
 
