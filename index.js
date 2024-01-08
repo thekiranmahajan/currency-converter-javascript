36d9097b5c8934f4a041e845a5e6669a2653c0d1
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-08 12:35:12 +0530

    trying to poulate option in select tag from API currency keys

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const inputCurrency = document.getElementById("from-currency");
 const outputCurrency = document.getElementById("to-currency");
 const conversionInfo = document.getElementById("conversion-info");
 
+const createOptions = (currencyObj, from, to) => {
+  from.innerHTML = "";
+  to.innerHTML = "";
+  for (const currency in currencyObj) {
+    const optionFrom = document.createElement("option");
+    optionFrom.value = currency;
+    optionFrom.text = currency;
+    from.appendChild(optionFrom);
+
+    const optionTo = document.createElement("option");
+    optionTo.value = currency;
+    optionTo.text = currency;
+    to.appendChild(optionTo);
+  }
+};
+
 const exchangeCurrency = async () => {
-  if (inputCurrency !== 0 && inputCurrency !== "") {
-    try {
+  try {
+    const input = parseFloat(inputCurrency.value);
+    if (!isNaN(input) && input !== 0) {
       const url = `https://v6.exchangerate-api.com/v6/4454f9cd9a5f86f42e1e0a43/latest/${from.value}`;
       fetch(url)
         .then((res) => res.json())
@@ -17,10 +34,14 @@ const exchangeCurrency = async () => {
           conversionInfo.innerText = `1 ${from.value} = ${
             data.conversion_rates[to.value]
           } ${to.value}`;
+          createOptions(data.conversion_rates, from, to);
         });
-    } catch (error) {
-      conversionInfo.innerText = `Oops something went wrong...`;
+    } else {
+      conversionInfo.innerText = `Please enter a valid amount to convert`;
+      outputCurrency.value = "";
     }
+  } catch (error) {
+    conversionInfo.innerText = `Oops something went wrong...`;
   }
 };
 exchangeCurrency();
